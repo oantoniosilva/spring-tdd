eb2362ddf0fceafa053b40fdef0f94774cdc8e7f
Antonio <antoniopsilva083@gmail.com>
2023-06-07 01:58:35 -0300

    test: create unit test of user class

---
+++ src/test/java/com/antonio/springtdd/RegisterApplicationTests.java
@@ -0,0 +1,34 @@
+package com.antonio.springtdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+	@Mock
+	private UserRepository userRepository;
+
+	@InjectMocks
+	private UserService userService;
+
+	@Test
+	void should_register_user_successfully() {
+		User userBeforeSave = new User("John", "1234", LocalDate.of(1993, 5, 14));
+		User userAfterSave = userBeforeSave;
+		userAfterSave.setId("1");
+		Mockito.when(userRepository.save(userBeforeSave)).thenReturn(userAfterSave);
+
+		var user = userService.register(userBeforeSave);
+
+		Mockito.verify(userRepository).save(userBeforeSave);
+		Assertions.assertEquals(userAfterSave, user);
+	}
+
+}
